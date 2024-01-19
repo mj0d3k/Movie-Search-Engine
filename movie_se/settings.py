7b10f2aa805578c4300686bacd4a83799ce30f8b
Ania Oleksy <a.oleksy.m@gmail.com>
2024-01-19 15:03:03 +0100

    app in settings (before migration)

--- movie_se/settings.py
+++ movie_se/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'search_engine',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
