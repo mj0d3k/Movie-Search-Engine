ed216840f63b2823f84e9eae06caf8ddf8af0519
Ania Oleksy <a.oleksy.m@gmail.com>
2024-01-20 15:26:18 +0100

    agata changes, before main codes/ FINAL DATABASE!!!

--- search_engine/admin.py
+++ search_engine/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Test
+from .models import Movie
 from import_export import resources, fields
 from import_export.admin import ImportExportModelAdmin
 
 
-class TestResource(resources.ModelResource):
+class MovieResource(resources.ModelResource):
     wikidata_id = fields.Field(attribute='wikidata_id', column_name='wikidata-id')
     review_score = fields.Field(attribute='review_score', column_name='review-score')
     rotten_tomatoes_id = fields.Field(attribute='rotten_tomatoes_id', column_name='rotten-tomatoes-id')
@@ -12,11 +12,11 @@ class TestResource(resources.ModelResource):
     wikipedia_link = fields.Field(attribute='wikipedia_link', column_name='wikipedia-link')
 
     class Meta:
-        model = Test
+        model = Movie
 
 
-class TestAdmin(ImportExportModelAdmin):
-    resource_class = TestResource
+class MovieAdmin(ImportExportModelAdmin):
+    resource_class = MovieResource
 
 
-admin.site.register(Test, TestAdmin)
+admin.site.register(Movie, MovieAdmin)
